600ea72d20c3cb43987d9088f5fc808532a4cda6
Jie-H89 <67579704+Jie-H89@users.noreply.github.com>
2022-01-23 22:40:50 +0800

    Add files via upload

---
+++ SCS.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts@4.4.2/token/ERC1155/ERC1155.sol";
+
+contract SCS is ERC1155 {
+    string public company;
+    string public date;
+    uint256 public shares;
+    address[] public founders;
+    uint NFT_ID = 0;
+    uint numConfirmationsRequired;
+    
+    constructor(string memory _comapny, string memory _date, uint256 _shares, address[] memory _founders, uint _numConfirmationsRequired)
+     ERC1155("https://abcoathup.github.io/SampleERC1155/api/token/{id}.json" ){
+    //genesis SCN 
+        shares = _shares;
+        _mint(msg.sender, NFT_ID, shares, "");
+
+        numConfirmationsRequired = _numConfirmationsRequired;
+        require(
+            numConfirmationsRequired > 0 &&
+                numConfirmationsRequired <= _founders.length,
+            "invalid number of required confirmations"
+        );
+
+
+    }
+    // ressiue
+    function ressiue(address _owner, uint256 _burn_amount, uint256 _mint_amount) public{
+        _burn(msg.sender, NFT_ID, _burn_amount);
+        _mint(msg.sender, NFT_ID, _mint_amount, "");
+        console.log("ressiue");
+    }
+    //Stock Transacting
+    function transfer(address target) public {
+        safeTransferFrom(msg.sender, target, NFT_ID, 1, "");
+        console.log("transfered");
+    }
+    //Stock Redemption
+    function redeem(address target) public {
+        safeTransferFrom(target, msg.sender, NFT_ID, 1, "");
+        _burn(msg.sender, NFT_ID, 1);
+        console.log("redeemed");
+    }
+
+}
